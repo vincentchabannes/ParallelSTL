3cfad0f3e3e0823049619470666ed1dc445afda9
Vincent Chabannes <vincent.chabannes@feelpp.org>
2024-02-28 21:23:44 +0100

    add an implementation of std::not_fn if not available

--- include/pstl/internal/glue_algorithm_impl.h
+++ include/pstl/internal/glue_algorithm_impl.h
@@ -12,6 +12,49 @@
 
 #include <functional>
 
+#if !__has_feature(cpp_lib_not_fn)
+namespace std
+{
+namespace detail {
+    template<class F>
+    struct not_fn_t {
+        F f;
+        template<class... Args>
+        auto operator()(Args&&... args)
+            noexcept(noexcept(!std::invoke(f, std::forward<Args>(args)...)))
+            -> decltype(!std::invoke(f, std::forward<Args>(args)...)) {
+            return !std::invoke(f, std::forward<Args>(args)...);
+        }
+
+        // cv-qualified overload for QoI
+        template<class... Args>
+        auto operator()(Args&&... args) const
+            noexcept(noexcept(!std::invoke(f, std::forward<Args>(args)...)))
+            -> decltype(!std::invoke(f, std::forward<Args>(args)...)) {
+            return !std::invoke(f, std::forward<Args>(args)...);
+        }
+
+        template<class... Args>
+        auto operator()(Args&&... args) volatile
+            noexcept(noexcept(!std::invoke(f, std::forward<Args>(args)...)))
+            -> decltype(!std::invoke(f, std::forward<Args>(args)...)) {
+            return !std::invoke(f, std::forward<Args>(args)...);
+        }
+        template<class... Args>
+        auto operator()(Args&&... args) const volatile
+            noexcept(noexcept(!std::invoke(f, std::forward<Args>(args)...)))
+            -> decltype(!std::invoke(f, std::forward<Args>(args)...)) {
+            return !std::invoke(f, std::forward<Args>(args)...);
+        }
+    };
+}
+
+template<class F>
+detail::not_fn_t<std::decay_t<F>> not_fn(F&& f) { return { std::forward<F>(f) }; }
+}
+#endif
+
+
 #include "pstl_config.h"
 
 #include "execution_defs.h"
